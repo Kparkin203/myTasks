c5ad6459ae7d6a0c770cbbeaa6fc0ef728db47d3
OpenShift guest <kparkin203@gmail.com>
2013-07-03 19:44:51 -0400

    rd2

--- js/main.js
+++ js/main.js
@@ -1,16 +1,42 @@
 // JavaScript Document
 
-var myTasks = ['Laundry', 'Cooking', 'Cleaning', 'Grocery', 'Workout'];
+var myTasks = [{
+    'name':'Laundry',
+    'complete':true
+}, {
+    'name': 'Wash the Car',
+	'complete': false	
+}, {
+	'name': 'Grocery Shopping',
+	'complete': true	
+}];
 
-var numbers = ['1', '2', '3', '4', '5'];
 
-//Listings all of my tasks for today
 
 document.write('<p>To Do List</p>');
 document.write('<ul>');
-for(var i = 0; i < numbers.length;  i++){
-for(var i = 0; i < myTasks.length;  i++){
-	document.write('<li>' + numbers[i] +' '+ myTasks[i] + '.</li>');
+
+for(var i = 0; i < myTasks.length; i++) {
+    try {
+       print(myTasks[i]);
+    } catch (error) {
+        console.log(error);
+    }
 }
-}
+
 document.write('</ul>');
+
+
+function print(theTask) {
+	    if(typeof theTask.name == 'undefined') {
+        throw 'The task name is missing!'
+    } else if (typeof theTask.complete == 'undefined') {
+        throw 'The task status is missing!'
+    } else {
+			if(theTask.complete) {
+       		 document.write('<li class="complete">' + theTask.name + '</li>');
+    		} else {
+        			document.write('<li>' + theTask.name + '</li>'); 
+    }
+}
+}
